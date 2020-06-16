c28fd8b6d27c9ce3c32ce957c7653e297cf9aa8b
Angel <angelhoikei@gmail.com>
2020-06-16 20:02:25 +0000

    trying out &&, || and ! logical operators

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,4 @@
-const raining = 1;
+const raining = true;
 const cold = "";
 const temperature = -1;
 
@@ -16,6 +16,7 @@ if(cold){
 console.log("Now you're ready to go outside!");
 */
 
+
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
@@ -25,3 +26,24 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+// Are you eligible to vote? AND && logical operator
+
+const isCitizen = true;
+const age = 21;
+
+if ( isCitizen && age > 18){
+  console.log("You are eligible to vote.");
+}
+
+// Is the weather okay to go out? OR || logical operator
+
+if (temperature < -40 || temperature > 40){
+  console.log("Maybe going outside isn't such a great idea...");
+}
+
+// Is it raining? NOT ! logical operator
+
+if (!raining) {
+  console.log("Leave your umberlla at home!");
+}
