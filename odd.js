59a7d154e2753212b04fca544009d09f645680fa
Angel <angelhoikei@gmail.com>
2020-06-17 20:42:06 +0000

    Create isOdd function

---
+++ odd.js
@@ -0,0 +1,10 @@
+let isOdd = function (num) {
+  return num % 2 !== 0;
+}
+
+console.log(isOdd(9));
+console.log(isOdd(14));
+console.log(isOdd(87));
+
+console.log(" 3 is odd: " + isOdd(3));
+console.log(" 8 is odd: " + isOdd(8));
