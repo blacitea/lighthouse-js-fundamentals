24c31a525301bd98e274c07ccd2fb14cc03b2de2
Angel <angelhoikei@gmail.com>
2020-06-19 20:55:02 +0000

    concat and merge array practice.

---
+++ concatarray.js
@@ -0,0 +1,15 @@
+const concat = ( array1, array2) => {
+  let newArray = [];
+  array1.forEach( element => {
+    newArray.push( element);
+  });
+  array2.forEach(element => {
+    newArray.push(element);
+  });
+  return newArray;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
